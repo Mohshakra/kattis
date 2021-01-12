17d774818cb9d7ab60f52f41157cdc8f57648b8c
mohshakra <shakramohammed@gmail.com>
2021-01-12 07:50:38 +0100

    Free_food

---
+++ Free_food/Free_food.cpp
@@ -0,0 +1,41 @@
+// länk : https://open.kattis.com/problems/freefood
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+
+
+
+
+    return 0;
+}
+
+
+
+/**
+ * Algorithm
+ * 
+ * Take in the quentity of the events.
+ * Take in first events days.
+ * Fyll up the days in an array.
+ * take in the second events days.
+ * fyll up the same array and check if the numbers already exists.
+ * continue to do the same on all events.
+ * return the length of the array.
+ * 
+*/
+
+
+/**
+ * input cases
+ * 
+ * 3
+ * 10 14
+ * 13 17
+ * 25 26
+ * -> 10
+*/
