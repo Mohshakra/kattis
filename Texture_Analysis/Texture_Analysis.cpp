1ac24fa928d54cdfb15902f47cd9bfe83f31522d
mohshakra <shakramohammed@gmail.com>
2021-01-20 17:00:48 +0100

    Texture works fine

--- Texture_Analysis/Texture_Analysis.cpp
+++ Texture_Analysis/Texture_Analysis.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
 #include <vector> 
 
-
 using namespace std;
 
 int occurence = 0;
 string checker(vector <int> &amount){
-
 		if(amount.size() == 1){
 			return "EVEN";
 		}
 		else
 		{
 			for(int i = 2 ; i < amount.size() ; i++){
-				
 				if(amount[1]== amount[i])
 					continue ;
 				else{
 					return "NOT EVEN" ;
 				}
-				
 			}
 			return "EVEN";
-	
-		}
-	
-			
+		}			
 }
 
 int main(){
-
 	vector <char> line;
 	vector <int> amount;
 	vector <string> result;
@@ -37,62 +29,51 @@ int main(){
 	char test_char;
 	int first_occur = 0 ;
 
-	
-
-
-
 	while(1){
 		test_char=fgetc(stdin);
-		
-		if(test_char != '\n' ){
+
+		if((test_char != 'E') && (test_char != '\n')){
 			line.push_back(test_char);
-			
 			if(test_char == '.'){
 				first_occur++;
 				continue ;
 			}
-			
 			else if((test_char =='*') && ((first_occur!=0))) {
 				amount.push_back(first_occur);
 				first_occur = 0;
-
 			}
 			else if (test_char=='*')
 			{
 				amount.push_back(0) ;
 				first_occur = 0 ;
-				
 			}
 		}
+		else if(test_char == '\n'){
+				result.push_back(checker(amount));
+				amount.clear();
+				continue ; 
+			}
 		else{
-			
 			break;
 		}
 	}
 	int size_of_amount = amount.size();
-	cout << "\n";
-	cout << "the size is : " << size_of_amount;
-	cout << "\n";
 
-	cout << "\n";
-	cout << "the checker is :" <<  checker(amount);
-	cout << "\n";
+	// cout << "\n";
+	// cout << "the size is : " << size_of_amount;
+	// cout << "\n";
 
+	// cout << "\n";
+	// cout << "the checker is :" <<  checker(amount);
+	// cout << "\n";
 
-
-	for(int i = 0 ; i < size_of_amount; i++){
-				cout << amount[i];
-			cout << "\n" ;
+	for(int i = 0 ; i < result.size(); i++){
+				// cout << amount[i];
+				cout << i + 1  <<  " "  << result[i];
+			 	cout << "\n" ;
 			}
 
-	for(int i = 0 ; i <= line.size(); i++ )
-			cout << line[i] ;
-
-	
-		
-
 	return 0;
-
 }
 
 
