00c17a6aafd6a1d84de970c34b535c9744dc7d7a
mohshakra <shakramohammed@gmail.com>
2021-01-21 14:38:05 +0100

    Troll_hunt done

--- Troll_hunt/Troll_hunt.cpp
+++ Troll_hunt/Troll_hunt.cpp
@@ -8,7 +8,25 @@ int main(){
 
     int stones, knights, knights_g ;
     cin >> stones >> knights >> knights_g ;
+    stones = stones - 1 ;
+    int groups;
+    int days ;
+    int rest_stones ;
 
+    groups = knights/knights_g ;
+    days = stones/groups ;
+    rest_stones = stones%groups ;
+
+    // cout << "\n" << "this is rest stones" << rest_stones << "\n" ;
+
+    if(rest_stones > 0) { 
+        cout << days + 1;
+    }
+    else{
+        cout  << days  ;
+    }
+
+    
     
 
 
