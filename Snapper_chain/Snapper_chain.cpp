75c0b0de2f0b8ce75c23d6efa428a2922958a1fc
mohshakra <shakramohammed@gmail.com>
2021-02-18 13:09:08 +0100

    Get the logic working

--- Snapper_chain/Snapper_chain.cpp
+++ Snapper_chain/Snapper_chain.cpp
@@ -11,13 +11,15 @@ int tunnel(vector<int> &arr){
             return i ;
         }
     }
-    return -1 ;
+    return -2 ;
 }
 /**
- * It take an array, 
+ * it flips the 0 to 1 and vice versa. when the array is ones then flip everythings to zeros.
+ * otherwise it flips the ones until first zero.
+ * return the ticked array.
 */
-void tick(vector<int> &arr , int index){
-    if(index != -1){
+vector<int> tick(vector<int> &arr , int index){
+    if(index != -2){
         for(int i = 0 ; i <= index ; i++){
             arr[i] = !arr[i] ; 
             }
@@ -27,14 +29,16 @@ void tick(vector<int> &arr , int index){
             arr[i] = !arr[i];
         }
     }
+    return arr ;
+
 }
 
-string checker(int value){
-    if(value == 1){
-        return "ON";
+string checker(vector<int> &arr ){
+    if(tunnel(arr) != -2 || arr.size() ==0){
+        return "OFF";
     }
     else {
-        return "OFF";
+        return "ON";
     }
 }
 
@@ -42,25 +46,46 @@ string checker(int value){
 
 int main(){
 
-    vector <int> arrs = {0,1,0} ;
-    //tick(arrs, 0) ;
-    cout << "Tunnel value : " ;
-    cout << tunnel(arrs) << "\n"  ;
-    
-    tick(arrs ,tunnel(arrs));
+    vector <int> arrs ;
+    int number_of_devices , number_of_attemps;
+    cin >> number_of_devices;
+    cin >> number_of_attemps;
+    for(int i = 0; i < number_of_devices ; i++){
+        arrs.push_back(0);
+    }
+
+
+    vector <int> arrs_temp = arrs;
 
     cout << "v = { ";
-    for (int n : arrs) {
-        std::cout << n << ",";
+        for (int n : arrs_temp ) {
+            std::cout << n << ",";
+        }
+    cout << "} \n";    
+
+
+
+    int count = 0 ; 
+    vector <int> to_print ;
+    while(count < number_of_attemps){
+        to_print = tick(arrs_temp ,tunnel(arrs_temp)); 
+        cout << "v = { ";
+        for (int n : to_print ) {
+            std::cout << n << ",";
+        }
+        cout << "} \n";
+        to_print = to_print;
+        count++ ;
     }
-    cout << "} \n";
+    
+    cout << checker(to_print);
+
     return 0;
 }
 
 /**
  * Algorithm
- * when tick, flip the first elemet.
- * Check the second elemet and flip it if 
+ * 
 */
 
 /**
